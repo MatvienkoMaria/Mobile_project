d06ceffe907e172d7a80be8874bd528584ff7d84
MatvienkoMaria <nyasha5555@gmail.com>
2023-05-18 17:21:37 +0300

    Added changes to onCreate() method in AddGroupActivity class.

--- app/src/main/java/com/mobile/project/AddGroupActivity.java
+++ app/src/main/java/com/mobile/project/AddGroupActivity.java
@@ -4,13 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddGroupActivity extends AppCompatActivity {
 
+    private ArrayList<String> groups = new ArrayList<>(Arrays.asList("РЛ-221","РЛ-222"));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_group);
+
+        TextView textSelectedGroup = findViewById(R.id.textSelectedGroup);
+        Spinner spinner = findViewById(R.id.spinner);
+        //Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, groups);
+        //Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Применяем адаптер к элементу spinner
+        spinner.setAdapter(adapter);
+
+        AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                // Получаем выбранный объект
+                String item = (String)parent.getItemAtPosition(position);
+                textSelectedGroup.setText(item);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        };
+        spinner.setOnItemSelectedListener(itemSelectedListener);
     }
 
     public void backToCalendarFromAddGroup(View view) {
