7b7c87e340c639907d03521f6b2985b69db5509c
MatvienkoMaria <nyasha5555@gmail.com>
2023-05-18 15:55:45 +0300

    Added action to connect from controlling page to settings page.

--- app/src/main/java/com/mobile/project/ControllingActivity.java
+++ app/src/main/java/com/mobile/project/ControllingActivity.java
@@ -2,7 +2,9 @@ package com.mobile.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ControllingActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ControllingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.controlling);
+    }
+
+    public void toSettingsAction(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
