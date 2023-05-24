bccc2e386a46874ca80eb83e6474b3f4319408e6
User <theendriu010203@gmail.com>
2023-05-24 19:51:48 +0300

    added connection to server
    (cherry picked from commit a96f6e46cecc90d4434492cff6b3c02eb20ca2d6)

--- app/src/main/java/com/mobile/project/pojo/Subject.java
+++ app/src/main/java/com/mobile/project/pojo/Subject.java
@@ -1,21 +1,29 @@
 package com.mobile.project.pojo;
 
+import java.util.List;
+
 public class Subject {
     public String name;
+    public String time;
     public String teacher;
     public String timeStart;
     public String timeEnd;
-    public String dayMonth;
-    public String typeOfLesson;
+    public String day_of_week;
     public String subgroup_name;
+    public String room;
+    public List<String> date;
+    public String type;
+    public String dayMonth;
 
-    public Subject(String name, String teacher, String timeStart, String timeEnd, String dayMonth, String typeOfLesson, String subgroup_name) {
+    public Subject(){};
+
+    public Subject(String name, String teacher, String timeStart, String timeEnd, String dayMonth, String type, String subgroup_name) {
         this.name = name;
         this.teacher = teacher;
         this.timeStart = timeStart;
         this.timeEnd = timeEnd;
         this.dayMonth = dayMonth;
-        this.typeOfLesson = typeOfLesson;
+        this.type = type;
         this.subgroup_name = subgroup_name;
     }
 }
